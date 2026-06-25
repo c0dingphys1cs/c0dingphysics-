516345058b31d09bd924580cbe89f27bf446c9da
Swati Devi <future95887@gmail.com>
2026-06-25 06:43:50 +0530

    First python upload

---
+++ python_basics.py
@@ -0,0 +1,11 @@
+#Basic variables and math operations
+name="Physics"
+age=24
+
+#Basic calculations
+mass=15  #kg
+acceleration =9.8 #m/s^2
+force=mass*acceleration
+
+print(f"name:{name}")
+print(f"force={force}Newtons")
